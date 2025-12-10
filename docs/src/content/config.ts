4d564eea11ea5fe3f0dd05a6034f873942406d5d
Mikołaj Bońkowski <134430453+Bonkowskii@users.noreply.github.com>
2025-12-10 13:46:07 +0100

    Update config.ts

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,6 +1,8 @@
-// docs/src/content/config.ts
-import { docsSchema, docsLoader } from '@astrojs/starlight/schema';
+// src/content/config.ts
+
 import { defineCollection } from 'astro:content';
+import { docsLoader } from '@astrojs/starlight/loaders';
+import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
   docs: defineCollection({
